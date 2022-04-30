1491bc2e58383203ff1d03b86dc9a6cc1eaacfa3
Aftab Khan <aftabsaboorkhan@gmail.com>
2022-05-01 03:09:28 +0530

    url issue fixed

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const cors = require('cors');
+
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 const connectDb = require("./config/db");
 connectDb();
+// Cors 
+const corsOptions = {
+  origin: process.env.ALLOWED_CLIENTS.split(',')
+  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+}
 
+// Default configuration looks like
+// {
+//     "origin": "*",
+//     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     "preflightContinue": false,
+//     "optionsSuccessStatus": 204
+//   }
+
+app.use(cors(corsOptions))
 app.use(express.json());
 
 app.set("views", path.join(__dirname, "/views"));
